138d8c0338d310966fd976d3b796d757bc9bb4e6
Fabricio Ribeiro Ferreira <frferreira@msn.com>
2026-05-21 01:29:55 +0000

    Add files via upload

---
+++ aula-14.1/script.js
@@ -0,0 +1,21 @@
+document.writeln("Olá, mundo!");
+document.writeln("<h2>Bem-vindo ao JavaScript</h2>");
+//window.alert("Esta é uma mensagem de alerta!");
+document.getElementById("title").innerHTML = "Aprendendo JavaScript com o melhor professor da UVV!!!";
+
+let nome = "" //prompt("Digite seu nome:");
+
+const novafrase = ocument.getElementById("novafrase");
+
+novafrase.innerHTML = "Olá, " + nome + "! Seja bem-vindo ao JavaScript!";
+
+novafrase.style.color = "blue";
+novafrase.style.fontSize = "80px";
+
+
+function exibirMensagem() {
+    let nome = document.getElementById("nome").value;
+    novafrase.innerHTML = "Olá, " + nome + "! Seja bem-vindo ao JavaScript!";
+    novafrase.style.color = "green";
+    novafrase.style.fontSize = "50px";
+}
